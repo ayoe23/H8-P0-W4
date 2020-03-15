b04b0fc64866ed51913680d9cabc6e4760573d10
michaelm96 <michaelarceus96@gmail.com>
2020-03-15 20:35:36 +0700

    highestScore

---
+++ E15_highScore.js
@@ -0,0 +1,89 @@
+function highestScore (students) {
+    
+    var objHasil = {};
+
+    for (var i = 0; i < students.length; i++)
+    {
+        objHasil[students[i].class] = {} ;
+        
+        var temp = 0;
+        for (var j = 0; j < students.length; j++)
+        {
+            if (students[i].class == students[j].class  &&  students[j].score > temp)
+            {
+                objHasil[students[i].class].name = students[j].name;
+                temp = students[j].score;
+                objHasil[students[i].class].score = temp;
+            }
+            
+        }
+    }
+    return objHasil;
+  }
+  
+  // TEST CASE
+  console.log(highestScore([
+    {
+      name: 'Dimitri',
+      score: 90,
+      class: 'foxes'
+    },
+    {
+      name: 'Alexei',
+      score: 85,
+      class: 'wolves'
+    },
+    {
+      name: 'Sergei',
+      score: 74,
+      class: 'foxes'
+    },
+    {
+      name: 'Anastasia',
+      score: 78,
+      class: 'wolves'
+    }
+  ]));
+  
+  // {
+  //   foxes: { name: 'Dimitri', score: 90 },
+  //   wolves: { name: 'Alexei', score: 85 }
+  // }
+  
+  
+  console.log(highestScore([
+    {
+      name: 'Alexander',
+      score: 100,
+      class: 'foxes'
+    },
+    {
+      name: 'Alisa',
+      score: 76,
+      class: 'wolves'
+    },
+    {
+      name: 'Vladimir',
+      score: 92,
+      class: 'foxes'
+    },
+    {
+      name: 'Albert',
+      score: 71,
+      class: 'wolves'
+    },
+    {
+      name: 'Viktor',
+      score: 80,
+      class: 'tigers'
+    }
+  ]));
+  
+  // {
+  //   foxes: { name: 'Alexander', score: 100 },
+  //   wolves: { name: 'Alisa', score: 76 },
+  //   tigers: { name: 'Viktor', score: 80 }
+  // }
+  
+  
+  console.log(highestScore([])); //{}
